fccdf4f15ecee8b70a23fc6adac66822b2aa0236
Florian Spier <mail@florian-spier.de>
2021-02-19 09:55:09 +0100

    Docs: add schema

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -68,21 +68,29 @@ function Home() {
         >
             <header className={clsx('hero hero--primary', styles.heroBanner)}>
                 <div className="container">
-                    <h1 className="hero__title">{siteConfig.title}</h1>
-                    <p className="hero__subtitle">{siteConfig.tagline}</p>
-                    <div className={styles.buttons}>
-                        <Link
-                            className={clsx(
-                                'button button--outline button--secondary button--lg',
-                                styles.getStarted
-                            )}
-                            to={useBaseUrl('docs/intro')}
-                        >
-                            Get Started
-                        </Link>
+                    <div className="mrx-flex">
+                        <div>
+                            <h1 className="hero__title">{siteConfig.title}</h1>
+                            <p className="hero__subtitle">{siteConfig.tagline}</p>
+                            <div className={styles.buttons}>
+                                <Link
+                                    className={clsx(
+                                        'button button--outline button--secondary button--lg',
+                                        styles.getStarted
+                                    )}
+                                    to={useBaseUrl('docs/intro')}
+                                >
+                                    Get Started
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="mrx-schema-container">
+                            <img className="mrx-schema" src="img/mini-rx-store-schema.svg" />
+                        </div>
                     </div>
                 </div>
             </header>
+
             <main>
                 {features && features.length > 0 && (
                     <section className={styles.features}>
