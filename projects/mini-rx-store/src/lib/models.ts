c46a09190572d9055ef675dc1afdfdbc9c6fe868
Florian Spier <mail@florian-spier.de>
2021-12-08 08:54:28 +0100

    refactor(models, redux-devtools extension): Remove code emitting TS code: enums -> const enum

--- projects/mini-rx-store/src/lib/models.ts
+++ projects/mini-rx-store/src/lib/models.ts
@@ -5,7 +5,7 @@ export const enum ExtensionSortOrder {
     // The Undo Extension Meta Reducer should be the last one to be executed before "normal" reducers (for performance)
     // Reason: The Undo Extension Meta Reducers may send many Actions through all following Reducers to undo an Action
     // Also, we want to prevent that the replay of Actions shows up e.g. in the LoggerExtension Meta Reducer
-    UNDO_EXTENSION,
+    UNDO_EXTENSION = 1,
 }
 
 export interface AppState {
