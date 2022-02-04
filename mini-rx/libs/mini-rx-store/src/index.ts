396bff94aeac54308d529fb98fe4e8d57cf3d20a
Florian Spier <mail@florian-spier.be>
2022-02-04 11:00:44 +0100

    refactor(all): Refactor to NX workspace: move mini-rx-store lib to ts lib
    See: https://github.com/spierala/mini-rx-store/issues/67

--- mini-rx/libs/mini-rx-store/src/index.ts
+++ mini-rx/libs/mini-rx-store/src/index.ts
@@ -1,1 +1,25 @@
-export * from './lib/mini-rx-store';
+/*
+ * Public API Surface of mini-rx-store
+ */
+
+export { actions$, Store, configureStore, createFeatureStore } from './lib/store';
+export { default as _StoreCore } from './lib/store-core';
+export { FeatureStore } from './lib/feature-store';
+export { createFeatureSelector, createSelector } from './lib/selector';
+export {
+  Action,
+  Reducer,
+  Actions,
+  ReducerDictionary,
+  StoreConfig,
+  FeatureStoreConfig,
+  StoreExtension
+} from './lib/models';
+export { ofType } from './lib/utils';
+export {
+  ReduxDevtoolsExtension,
+  ReduxDevtoolsOptions,
+} from './lib/extensions/redux-devtools.extension';
+export { LoggerExtension } from './lib/extensions/logger.extension';
+export { ImmutableStateExtension } from './lib/extensions/immutable-state.extension';
+export { UndoExtension, undo } from './lib/extensions/undo.extension';
