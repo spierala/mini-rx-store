872bcc30ce42b163fe120183994d67072e3a7f34
Florian Spier <mail@florian-spier.be>
2022-03-24 23:14:01 +0100

    refactor(all): Refactor to NX workspace: cleanup

--- trash/jest.base.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'jest-preset-angular',
-    setupFilesAfterEnv: ['<rootDir>/../../jest.base.setup.ts'],
-    verbose: true,
-    transform: {
-        '\\.(ts|js)x?$': 'ts-jest',
-    },
-};
