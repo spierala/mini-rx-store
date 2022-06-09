dafbf6cb8bf3335e86a294435b696f9a510158f6
Florian Spier <mail@florian-spier.de>
2022-06-09 22:23:32 +0200

    fix(mini-rx-store): log setState callback result in devtools and logger extension

--- libs/mini-rx-store/src/lib/utils.ts
+++ libs/mini-rx-store/src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { OperatorFunction, pipe } from 'rxjs';
 import { distinctUntilChanged, filter, map } from 'rxjs/operators';
-import { Action } from './models';
+import { Action, ActionWithPayload, AppState } from './models';
+import { MiniRxActionType, SetStateAction } from './actions';
 
 export const miniRxNameSpace = '@mini-rx';
 
@@ -23,4 +24,32 @@ export function miniRxError(message: string): never {
 // This isn't a real GUID!
 export function generateId() {
     return Math.random().toString(36).slice(2);
+}
+
+const key: keyof SetStateAction<any> = '__internalType';
+const type: MiniRxActionType = 'set-state';
+
+// Type predicate
+export function isSetStateAction<T>(action: Action): action is SetStateAction<T> {
+    return action[key] === type;
+}
+
+export function beautifyActionsForLogging(action: Action, state: AppState): Action {
+    if (isSetStateAction(action)) {
+        return mapSetStateActionToActionWithPayload(action, state);
+    }
+    return action;
+}
+
+function mapSetStateActionToActionWithPayload(
+    action: SetStateAction<any>,
+    state: AppState
+): ActionWithPayload {
+    const stateOrCallback = action.stateOrCallback;
+    const featureState = state[action.featureKey];
+    return {
+        type: action.type,
+        payload:
+            typeof stateOrCallback === 'function' ? stateOrCallback(featureState) : stateOrCallback,
+    };
 }
