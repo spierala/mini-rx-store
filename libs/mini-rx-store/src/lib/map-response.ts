767e64c2b2998110ee7b4e4ccff19a8dcf9007e1
Florian Spier <mail@florian-spier.de>
2022-09-02 20:33:43 +0200

    refactor(mini-rx-store): mapResponse: improve typings

--- libs/mini-rx-store/src/lib/map-response.ts
+++ libs/mini-rx-store/src/lib/map-response.ts
@@ -1,11 +1,11 @@
 import { EMPTY, from, Observable, of } from 'rxjs';
-import { catchError, mergeMap, map } from 'rxjs/operators';
+import { catchError, map, mergeMap } from 'rxjs/operators';
 import { Action } from './models';
 
-export function mapResponse<T>(
-    mapFn: (next: T) => Action | Action[],
-    errorFn: (error: any) => Action | Action[] | void
-): (source: Observable<T>) => Observable<Action> {
+export function mapResponse<R1 extends Action, R2 extends Action>(
+    mapFn: (next: any) => R1 | R1[],
+    errorFn: (error: any) => R2 | R2[] | void
+): (source: Observable<any>) => Observable<R1 | R2> {
     return (source) =>
         source.pipe(
             map((v) => mapFn(v)),
