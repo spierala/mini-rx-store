3e496f41d6fcb1226b8a5a5f726dafa9b35d4ac8
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2022-11-17 20:24:18 +0100

    feat(command): custom metaReducer takes commands from root state and passes them further

--- libs/mini-rx-store/src/lib/extensions/command/command-extension.ts
+++ libs/mini-rx-store/src/lib/extensions/command/command-extension.ts
@@ -1,22 +1,46 @@
 import { Action, AppState, Reducer, StoreExtension } from '../../models';
 import StoreCore from '../../store-core';
+import { Cmd, NOOP_CMD, RootReducerWithCmds, unwrapCmds, withCmd } from './command-models';
+
+type ProcessCommandsCallback = (commands: Cmd[]) => void;
 
 export class CommandExtension extends StoreExtension {
     init(): void {
         //console.log('CommandExtension initialising');
 
-        const commandMetaReducer = createCommandMetaReducer();
+        const processCommands: ProcessCommandsCallback = (cmds) => {
+            this.process(cmds);
+        };
+
+        const commandMetaReducer = createCommandMetaReducer(processCommands);
 
         StoreCore.addMetaReducers(commandMetaReducer);
     }
+
+    async process(commands: Cmd[]) {
+        const relevantCmds = commands.filter((cmd) => cmd !== NOOP_CMD);
+
+        console.log(`Received ${commands.length} command(s), ${relevantCmds.length} relevant`);
+    }
 }
 
-function createCommandMetaReducer() {
+function createCommandMetaReducer(processCommands: ProcessCommandsCallback) {
     function commandMetaReducer(reducer: Reducer<AppState>): Reducer<AppState> {
         //console.log('commandMetaReducer invoked');
 
         function newReducer(state: AppState, action: Action): AppState {
-            return reducer(state, action);
+            const stateWithCmds = withCmd(state);
+
+            const nextStateWithCmds = (reducer as RootReducerWithCmds<AppState, Action>)(
+                stateWithCmds,
+                action
+            );
+
+            const { state: nextState, cmds: commands } = unwrapCmds(nextStateWithCmds);
+
+            processCommands([]);
+
+            return nextState;
         }
 
         return newReducer;
