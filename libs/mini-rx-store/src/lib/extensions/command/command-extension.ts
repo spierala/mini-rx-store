f121498891a3c042ffa4b8efb9b66b73218e2126
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2022-11-17 20:15:46 +0100

    refactor(command): prepare a function to create custom metaReducer

--- libs/mini-rx-store/src/lib/extensions/command/command-extension.ts
+++ libs/mini-rx-store/src/lib/extensions/command/command-extension.ts
@@ -3,18 +3,24 @@ import StoreCore from '../../store-core';
 
 export class CommandExtension extends StoreExtension {
     init(): void {
-        console.log('CommandExtension initialising');
+        //console.log('CommandExtension initialising');
+
+        const commandMetaReducer = createCommandMetaReducer();
 
         StoreCore.addMetaReducers(commandMetaReducer);
     }
 }
 
-function commandMetaReducer(reducer: Reducer<AppState>): Reducer<AppState> {
-    console.log('commandMetaReducer invoked');
+function createCommandMetaReducer() {
+    function commandMetaReducer(reducer: Reducer<AppState>): Reducer<AppState> {
+        //console.log('commandMetaReducer invoked');
 
-    function newReducer(state: AppState, action: Action): AppState {
-        return reducer(state, action);
+        function newReducer(state: AppState, action: Action): AppState {
+            return reducer(state, action);
+        }
+
+        return newReducer;
     }
 
-    return newReducer;
+    return commandMetaReducer;
 }
