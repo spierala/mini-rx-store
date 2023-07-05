72a06f73ae215ea83472992eaf269bd8ab468084
Florian Spier <mail@florian-spier.be>
2023-07-05 22:28:43 +0200

    feat(signal-store): signal store: specs

--- libs/signal-store/src/lib/ng-modules/store.module.ts
+++ libs/signal-store/src/lib/ng-modules/store.module.ts
@@ -1,5 +1,5 @@
 import { Inject, InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
-import { Actions, FeatureConfig, Reducer, StoreConfig } from '../models';
+import { Actions, AppState, FeatureConfig, Reducer, StoreConfig } from '../models';
 import { Store } from '../store';
 import { actions$, addFeature } from '../store-core';
 
@@ -9,6 +9,10 @@ export const FEATURE_REDUCERS = new InjectionToken<Reducer<any>[]>('@mini-rx/fea
 export const FEATURE_CONFIGS = new InjectionToken<FeatureConfig<any>[]>(
     '@mini-rx/feature-store-config'
 );
+
+export function storeFactory(config: StoreConfig<AppState>) {
+    return new Store(config);
+}
 
 @NgModule()
 export class StoreRootModule {
@@ -39,10 +43,14 @@ export class StoreModule {
             providers: [
                 { provide: STORE_CONFIG, useValue: config },
                 {
+                    provide: Store,
+                    useFactory: storeFactory,
+                    deps: [STORE_CONFIG],
+                },
+                {
                     provide: Actions,
                     useValue: actions$,
                 },
-                Store,
             ],
         };
     }
--- libs/signal-store/src/lib/store.ts
+++ libs/signal-store/src/lib/store.ts
@@ -1,15 +1,12 @@
 import { AppState, StoreConfig } from './models';
 import { configureStore, dispatch, rxEffect, selectableAppState } from './store-core';
-import { Inject, Injectable } from '@angular/core';
-import { STORE_CONFIG } from './ng-modules/store.module';
 
-@Injectable()
 export class Store {
     dispatch = dispatch;
     rxEffect = rxEffect;
     select = selectableAppState.select.bind(selectableAppState);
 
-    constructor(@Inject(STORE_CONFIG) config: StoreConfig<AppState>) {
+    constructor(config: StoreConfig<AppState>) {
         configureStore(config);
     }
 }
