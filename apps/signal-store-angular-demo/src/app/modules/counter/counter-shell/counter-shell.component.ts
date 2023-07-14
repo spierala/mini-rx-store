e78e890ce908d548bb2dd3c2d7df02fb128bf6d8
Florian Spier <mail@florian-spier.be>
2023-07-14 16:16:37 +0200

    feat(signal-store): signal store: remove demo code

--- apps/signal-store-angular-demo/src/app/modules/counter/counter-shell/counter-shell.component.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-    templateUrl: './counter-shell.component.html',
-    styleUrls: ['./counter-shell.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class CounterShellComponent {}
--- apps/signal-store-angular-demo/src/app/modules/products/models/product.ts
+++
@@ -1,9 +0,0 @@
-/* Defines the product entity */
-export class Product {
-    id: number | undefined;
-    productName: string = '';
-    productCode: string = 'New';
-    description: string = '';
-    starRating: number = 0;
-    price: number | undefined;
-}
