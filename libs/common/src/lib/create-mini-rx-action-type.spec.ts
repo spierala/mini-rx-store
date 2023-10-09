ebb1fdd1dfc4eac280190763872d148077463643
Florian Spier <mail@florian-spier.be>
2023-10-09 21:17:20 +0200

    feat(signal-store): common: refactor actions, tests

--- libs/common/src/lib/create-mini-rx-action-type.spec.ts
+++ libs/common/src/lib/create-mini-rx-action-type.spec.ts
@@ -4,16 +4,26 @@ describe('createMiniRxActionType', () => {
     it('should create a MiniRx Action Type', () => {
         const featureKey = 'todos';
 
-        const type = createMiniRxActionType(OperationType.INIT, featureKey);
-        expect(type).toBe('@mini-rx/todos/init');
+        const storeInit = createMiniRxActionType(OperationType.INIT);
+        expect(storeInit).toBe('@mini-rx/init');
 
-        const type2 = createMiniRxActionType(OperationType.DESTROY, featureKey);
-        expect(type2).toBe('@mini-rx/todos/destroy');
+        const featureInit = createMiniRxActionType(OperationType.INIT, featureKey);
+        expect(featureInit).toBe('@mini-rx/todos/init');
 
-        const type3 = createMiniRxActionType(OperationType.CONNECTION, featureKey, 'someKey');
-        expect(type3).toBe('@mini-rx/todos/connection/someKey');
+        const featureDestroy = createMiniRxActionType(OperationType.DESTROY, featureKey);
+        expect(featureDestroy).toBe('@mini-rx/todos/destroy');
 
-        const type4 = createMiniRxActionType(OperationType.SET_STATE, featureKey, 'someName');
-        expect(type4).toBe('@mini-rx/todos/set-state/someName');
+        const connection = createMiniRxActionType(OperationType.CONNECTION, featureKey, 'someKey');
+        expect(connection).toBe('@mini-rx/todos/connection/someKey');
+
+        const setState = createMiniRxActionType(OperationType.SET_STATE, featureKey);
+        expect(setState).toBe('@mini-rx/todos/set-state');
+
+        const setStateWithName = createMiniRxActionType(
+            OperationType.SET_STATE,
+            featureKey,
+            'someName'
+        );
+        expect(setStateWithName).toBe('@mini-rx/todos/set-state/someName');
     });
 });
