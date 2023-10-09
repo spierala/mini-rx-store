a1fc51e44346b3a09bc74c63f90c94ee92938188
Florian Spier <mail@florian-spier.be>
2023-10-09 21:50:42 +0200

    feat(signal-store): common: tests: ActionsOnQueue

---
+++ libs/common/src/lib/actions-on-queue.spec.ts
@@ -0,0 +1,34 @@
+import { ActionsOnQueue } from './actions-on-queue';
+import { take } from 'rxjs/operators';
+
+describe('ActionsOnQueue', () => {
+    it('should dispatch actions', () => {
+        const spy = jest.fn();
+
+        const actionsOnQueue = new ActionsOnQueue();
+        actionsOnQueue.actions$.subscribe(spy);
+
+        const action = { type: 'someAction' };
+        actionsOnQueue.dispatch(action);
+
+        expect(spy).toHaveBeenCalledWith(action);
+    }),
+        it('should queue actions', () => {
+            // Without queueScheduler this test would fail because of stack overflow (Read more here: https://blog.cloudboost.io/so-how-does-rx-js-queuescheduler-actually-work-188c1b46526e)
+
+            const callLimit = 5000;
+
+            const actionsOnQueue = new ActionsOnQueue();
+
+            const spy = jest.fn().mockImplementation(() => {
+                // Every received action dispatches another action
+                actionsOnQueue.dispatch({ type: 'someAction' });
+            });
+
+            actionsOnQueue.actions$.pipe(take(callLimit)).subscribe(spy);
+
+            actionsOnQueue.dispatch({ type: 'someInitialAction' }); // Dispatch an action to start the whole thing
+
+            expect(spy).toHaveBeenCalledTimes(callLimit);
+        });
+});
