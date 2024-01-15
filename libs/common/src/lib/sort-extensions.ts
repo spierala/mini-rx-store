6008062fdd5fb344e1f0a6f2384bccef231829de
Florian Spier <mail@florian-spier.be>
2024-01-16 00:41:31 +0100

    docs(common, signal-store): add comments

--- libs/common/src/lib/sort-extensions.ts
+++ libs/common/src/lib/sort-extensions.ts
@@ -1,6 +1,7 @@
 import { StoreExtension } from './models';
 
 export function sortExtensions<T extends StoreExtension>(extensions: T[]): T[] {
+    // TODO can be `toSorted` (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/toSorted) when available in TS
     return [...extensions].sort((a, b) => {
         return a.sortOrder - b.sortOrder;
     });
--- libs/signal-store/src/lib/utils.ts
+++ libs/signal-store/src/lib/utils.ts
@@ -1,3 +1,4 @@
+// TODO not needed anymore in Angular 17
 export function defaultSignalEquality(a: any, b: any) {
     return a === b;
 }
