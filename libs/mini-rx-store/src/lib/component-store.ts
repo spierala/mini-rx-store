403c688b684f1d03b632e4d20ab921bf41ff6e96
Karel De Smet <karel.desmet@verhaert.digital>
2024-05-02 06:44:06 +0200

    refactor(mini-rx-store): remove unused function mergeExtensions

--- libs/mini-rx-store/src/lib/component-store.ts
+++ libs/mini-rx-store/src/lib/component-store.ts
@@ -14,7 +14,6 @@ import {
     miniRxError,
     OperationType,
     Reducer,
-    sortExtensions,
     StateOrCallback,
     undo,
 } from '@mini-rx/common';
@@ -133,38 +132,6 @@ export class ComponentStore<StateType extends object>
         }
         super.destroy();
     }
-}
-
-// TODO remove after `calculateExtensions` is used
-function mergeExtensions(
-    global: ComponentStoreExtension[],
-    local: ComponentStoreExtension[]
-): ComponentStoreExtension[] {
-    // Local extensions overwrite the global extensions
-    // If extension is global and local => use local
-    // If extension is only global => use global
-    // If extension is only local => use local
-
-    const extensions: ComponentStoreExtension[] = [];
-    let globalCopy = [...global];
-    let localCopy = [...local];
-
-    global.forEach((globalExt) => {
-        local.forEach((localExt) => {
-            if (localExt.id === globalExt.id) {
-                // Found extension which is global and local
-                extensions.push(localExt); // Use local!
-                localCopy = localCopy.filter((item) => item.id !== localExt.id); // Remove found extension from local
-                globalCopy = globalCopy.filter((item) => item.id !== globalExt.id); // Remove found extension from global
-            }
-        });
-    });
-
-    return [
-        ...extensions, // Extensions which are global and local, but use local
-        ...localCopy, // Local only
-        ...globalCopy, // Global only
-    ];
 }
 
 export function createComponentStore<T extends object>(
