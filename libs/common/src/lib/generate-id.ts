1d233a9eecce80b4c30e146194a12469fc0a3777
Karel De Smet <karel.desmet@houseofhr.com>
2024-12-11 22:56:14 +0100

    refactor(common): refactor generateId to use Nano ID
    Refactor generateId due to a low chance of throwing a SyntaxError

--- libs/common/src/lib/generate-id.ts
+++ libs/common/src/lib/generate-id.ts
@@ -1,5 +1,16 @@
-// Simple alphanumeric ID: https://stackoverflow.com/a/12502559/453959
-// This isn't a real GUID!
-export function generateId(): string {
-    return Math.random().toString(36).slice(2);
+/*
+Implementation was copied from Nano ID (https://github.com/ai/nanoid)
+For more details, see LICENSE
+ */
+export const urlAlphabet = 'useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict';
+
+export function generateId(size = 6): string {
+    let id = '';
+    // A compact alternative for `for (var i = 0; i < step; i++)`.
+    let i = size | 0;
+    while (i--) {
+        // `| 0` is more compact and faster than `Math.floor()`.
+        id += urlAlphabet[(Math.random() * 64) | 0];
+    }
+    return id;
 }
