e271f2b3c4e74815dd96362d3edbc4a37ee67042
Karel De Smet <karel.desmet@houseofhr.com>
2024-12-14 23:09:47 +0100

    fix(common): pr comments

--- libs/common/src/lib/generate-id.spec.ts
+++ libs/common/src/lib/generate-id.spec.ts
@@ -1,25 +1,10 @@
 import { generateId, urlAlphabet } from './generate-id';
 
 describe('generateId', () => {
-    it('should generate an id of the given length', () => {
-        let id = generateId(1);
-
-        expect(id.length).toBe(1);
-
-        id = generateId(21);
-
-        expect(id.length).toBe(21);
-    });
-
     it('should generate an id of 6 characters long if no argument was passed', () => {
         const id = generateId();
 
         expect(id.length).toEqual(6);
-    });
-
-    it('should generate an id consisting of dashes, underscores, lowercase letters, uppercase letters or digits', () => {
-        const id = generateId(10);
-
         id.split('').forEach((char: string) => {
             expect(urlAlphabet.includes(char)).toBe(true);
         });
